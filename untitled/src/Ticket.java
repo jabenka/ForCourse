e29e4fc68f3c306d076c6d3f77bdac28ae399778
Denzel_Washington <150378576+jabenka@users.noreply.github.com>
2024-10-27 16:23:27 +0300

    Add files via upload

--- untitled/src/Ticket.java
+++ untitled/src/Ticket.java
@@ -61,4 +61,8 @@ public class Ticket {
                 ", price=" + price +
                 '}';
     }
+
+    public String getId() {
+        return id;
+    }
 }
--- untitled/src/TicketService.java
+++ untitled/src/TicketService.java
@@ -4,11 +4,12 @@ import java.util.*;
 import static java.lang.Math.abs;
 
 public class TicketService {
+
+    private static List<Ticket> tickets = new ArrayList<>();
     public static void main(String[] args) {
 
         Scanner in=new Scanner(System.in);
 
-        List<Ticket> tickets=new ArrayList<>();
 
         Ticket empty=new Ticket();
         tickets.add(empty);
@@ -16,7 +17,12 @@ public class TicketService {
         tickets.add(full);
         Ticket limited=new Ticket(createId(),"hallC",123,Instant.now());
         tickets.add(limited);
-        tickets.stream().forEach(System.out::println);
+
+        for(int i=111;i<118;i++ ){
+            tickets.add(new Ticket(createId(),"HallA",i,Instant.now(),true,'A',5.04,19.99));
+        }
+
+        tickets.forEach(System.out::println);
 
 
     }
@@ -43,5 +49,14 @@ public class TicketService {
 
         return ans.toString();
 
+    }
+
+    private static Ticket findById(String id){
+        for(Ticket ticket:tickets){
+            if(ticket.getId().equals(id)){
+                return ticket;
+            }
+        }
+        return null;
     }
 }
