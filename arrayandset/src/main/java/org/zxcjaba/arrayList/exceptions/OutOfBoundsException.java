2830de6dc5bcf810850c166a4668cd0e661bef69
Denzel_Washington <150378576+jabenka@users.noreply.github.com>
2024-11-18 20:10:26 +0300

    Add files via upload

---
+++ arrayandset/src/main/java/org/zxcjaba/arrayList/exceptions/OutOfBoundsException.java
@@ -0,0 +1,7 @@
+package org.zxcjaba.arrayList.exceptions;
+
+public class OutOfBoundsException extends RuntimeException {
+    public OutOfBoundsException(String message) {
+        super(message);
+    }
+}
