0fecac294dae7516fef2a1f7f2674d6a9d461fbf
Denzel_Washington <150378576+jabenka@users.noreply.github.com>
2024-11-18 20:09:26 +0300

    Delete src/main/java/org/zxcjaba directory

--- src/main/java/org/zxcjaba/ArrayList/Exceptions/OutOfBoundsException.java
+++
@@ -1,7 +0,0 @@
-package org.zxcjaba.ArrayList.Exceptions;
-
-public class OutOfBoundsException extends RuntimeException {
-    public OutOfBoundsException(String message) {
-        super(message);
-    }
-}
--- src/main/java/org/zxcjaba/HashSet/Exceptions/CollisionException.java
+++
@@ -1,7 +0,0 @@
-package org.zxcjaba.HashSet.Exceptions;
-
-public class CollisionException extends RuntimeException {
-    public CollisionException(String message) {
-        super(message);
-    }
-}
